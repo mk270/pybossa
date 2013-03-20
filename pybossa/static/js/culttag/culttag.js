42005c25603520d5afd4646c3a2bb9de0340ed09
Martin Keegan <martin@no.ucant.org>
2013-03-20 02:07:32 +0000

    move more javascript into own file

--- pybossa/static/js/culttag/culttag.js
+++ pybossa/static/js/culttag/culttag.js
@@ -11,8 +11,29 @@
     });
   };
 
+  function taskLoaded(task, deferred) {
+    if ( !$.isEmptyObject(task) ) {
+      // load image from flickr
+      var img = $('<img />');
+      img.load(function() {
+        // continue as soon as the image is loaded
+        deferred.resolve(task);
+      });
+      img.attr('src', task.info.url_b).css('height', 460);
+      img.addClass('img-polaroid');
+      task.info.image = img;
+    }
+    else {
+      deferred.resolve(task);
+    };
+  };
+
   culttag.loadUserProgress = function(module, short_name) {
 	return loadUserProgress(module, short_name);
+  };
+
+  culttag.taskLoaded = function(task, deferred) {
+	return taskLoaded(task, deferred);
   };
 
 } ( window.culttag = window.culttag || {}, jQuery ));
