f8db33f6d4e01cd020f0ba17f8ca5a0906ccbbd9
Martin Keegan <martin@no.ucant.org>
2013-03-20 02:20:10 +0000

    move task presenter to js library

--- pybossa/static/js/culttag/culttag.js
+++ pybossa/static/js/culttag/culttag.js
@@ -28,12 +28,55 @@
     };
   };
 
+  function _presentTask(task, deferred, module, short_name) {
+    if ( !$.isEmptyObject(task) ) {
+        culttag.loadUserProgress(module, short_name);
+        $('#photo-link').html('').append(task.info.image);
+        $("#photo-link").attr("href", task.info.link);
+        $("#question").html(task.info.question);
+        $('#task-id').html(task.id);
+        $('.btn-answer').off('click').on('click', function(evt) {
+            var answer = $(evt.target).attr("value");
+            if (typeof answer != 'undefined') {
+                //console.log(answer);
+                module.saveTask(task.id, answer).done(function() {
+                    deferred.resolve();
+                });
+                $("#loading").fadeIn(500);
+                if ($("#disqus_thread").is(":visible")) {
+                    $('#disqus_thread').toggle();
+                    $('.btn-disqus').toggle();
+                }
+            }
+            else {
+                $("#error").show();
+            }
+        });
+        $("#loading").hide();
+    }
+    else {
+        $(".skeleton").hide();
+        $("#loading").hide();
+        $("#finish").fadeIn(500);
+    }
+  };
+
+  function presentTask(module, short_name) {
+	return function(task, deferred) {
+      return _presentTask(task, deferred, module, short_name);
+	};
+  };
+
   culttag.loadUserProgress = function(module, short_name) {
 	return loadUserProgress(module, short_name);
   };
 
   culttag.taskLoaded = function(task, deferred) {
 	return taskLoaded(task, deferred);
+  };
+
+  culttag.presentTask = function(module, short_name) {
+	return presentTask(module, short_name);
   };
 
 } ( window.culttag = window.culttag || {}, jQuery ));
