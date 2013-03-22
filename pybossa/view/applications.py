812212c2637c41647b31c6bc616d1e9ac1dd55f9
Martin Keegan <martin@no.ucant.org>
2013-03-22 17:03:52 +0000

    move photo retriever out to own file

--- pybossa/view/applications.py
+++ pybossa/view/applications.py
@@ -34,32 +34,12 @@ from pybossa.util import Unique, Pagination, unicode_csv_reader, UnicodeWriter
 from pybossa.auth import require
 from pybossa.cache import apps as cached_apps
 
+from get_photos import get_flickr_photos
+
 import json
 
 blueprint = Blueprint('app', __name__)
 
-def get_flickr_photos(api_key, search_term, size="big"):
-    import europeana
-    """
-    Gets public photos from Flickr feeds
-    :arg string size: Size of the image from Flickr feed.
-    :returns: A list of photos.
-    :rtype: list
-    """
-    # Get the ID of the photos and load it in the output var
-    # add the 'ids': '25053835@N03' to the values dict if you want to
-    # specify a Flickr Person ID
-
-    # For each photo ID create its direct URL according to its size:
-    # big, medium, small (or thumbnail) + Flickr page hosting the photo
-    import json
-    srch = europeana.search.Search(api_key)
-    results =    srch.query(search_term)
-    photos = results
-    results = [ ("link", "url_b", "title") ]
-    for ph in photos["items"]:
-        results.append( (ph["link"], ph["edmPreview"][0], ph["title"][0]) )
-    return results
 
 class CSVImportException(Exception): pass
 
---
+++ pybossa/view/get_photos.py
@@ -0,0 +1,22 @@
+def get_flickr_photos(api_key, search_term, size="big"):
+    import europeana
+    """
+    Gets public photos from Flickr feeds
+    :arg string size: Size of the image from Flickr feed.
+    :returns: A list of photos.
+    :rtype: list
+    """
+    # Get the ID of the photos and load it in the output var
+    # add the 'ids': '25053835@N03' to the values dict if you want to
+    # specify a Flickr Person ID
+
+    # For each photo ID create its direct URL according to its size:
+    # big, medium, small (or thumbnail) + Flickr page hosting the photo
+    import json
+    srch = europeana.search.Search(api_key)
+    results =    srch.query(search_term)
+    photos = results
+    results = [ ("link", "url_b", "title") ]
+    for ph in photos["items"]:
+        results.append( (ph["link"], ph["edmPreview"][0], ph["title"][0]) )
+    return results
